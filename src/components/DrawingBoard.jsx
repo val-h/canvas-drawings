5b25e1f5b4fc2b3ef47bfee1bfe540864feb6a9f
Valentin Mirchev <valentin97mirchev@gmail.com>
2024-08-04 22:27:38 +0200

    First working iteration of undo logic

--- src/components/DrawingBoard.jsx
+++ src/components/DrawingBoard.jsx
@@ -5,6 +5,7 @@ import AvailablePointers from "./AvailablePointers";
 import { DEFAULT_COLOR, DEFAULT_POINTER } from "../enums";
 import ColorSelector from "./ColorSelector";
 
+// TODO: Add the PenIcon as a cursor
 const DrawingBoard = () => {
   const [context, setContext] = useState(null);
   const [isDrawing, setIsDrawing] = useState(false);
@@ -12,30 +13,94 @@ const DrawingBoard = () => {
   const [pointerSize, setPointerSize] = useState(DEFAULT_POINTER);
   const [strokeStyle, setStrokeStyle] = useState(DEFAULT_COLOR.hex);
 
+  // History state
+  const [actionHistory, setActionHistory] = useState([]);
+  const [redoHistory, setRedoHistory] = useState([]); // todo
+  const [currentAction, setCurrentAction] = useState({
+    color: strokeStyle,
+    pointerSize: pointerSize,
+    lines: [],
+  });
+
+  // redraw function - runtime complexity - o(n^2)
+  // try to find optimizations
+  const drawHistory = () => {
+    handleClearBoard();
+
+    // pop the latest action history to a redo stack
+    // check if pop would work with state variable if saved at the end
+    const lastAction = actionHistory[actionHistory.length];
+
+    for (let i = 0; i < actionHistory.length - 1; i++) {
+      const action = actionHistory[i];
+
+      // odd bug of stroke style changes on undo
+      context.strokeStyle = action.color;
+      action.lines.forEach((line) => {
+        console.log(line);
+        context.beginPath();
+        context.moveTo(line.moveTo.x, line.moveTo.y);
+        context.lineTo(line.lineTo.offsetX, line.lineTo.offsetY);
+        context.stroke();
+      });
+    }
+
+    setActionHistory(actionHistory.splice(0, actionHistory.length - 1));
+    setRedoHistory([lastAction, ...redoHistory]);
+  };
+
+  const handleUndo = () => {
+    drawHistory();
+  };
+
+  const handleRedo = () => {
+    // todo
+  };
+
   const handleMouseDown = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
     setIsDrawing(true);
     setLastPos({ x: offsetX, y: offsetY });
+    setCurrentAction({
+      color: strokeStyle,
+      pointerSize: pointerSize,
+      lines: [],
+    });
   };
+
+  //FIX: onMouseOut - stop drawing
   const handleMouseMove = (e) => {
     if (!isDrawing) return;
 
     const { offsetX, offsetY } = e.nativeEvent;
 
     if (isDrawing === true) {
+      //export to draw function
       context.beginPath();
       context.moveTo(lastPos.x, lastPos.y);
       context.lineTo(offsetX, offsetY);
       context.stroke();
+
+      //something of the sorts
+      setCurrentAction({
+        color: strokeStyle,
+        lines: [
+          ...currentAction.lines,
+          { moveTo: { ...lastPos }, lineTo: { offsetX, offsetY } },
+        ],
+      });
+
       setLastPos({ x: offsetX, y: offsetY });
     }
   };
+
   const handleMouseUp = (e) => {
     setIsDrawing(false);
+    setActionHistory([...actionHistory, currentAction]);
   };
 
   const handleClearBoard = () => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
   };
 
   const handlePointerSizeChange = (size) => {
@@ -53,6 +118,9 @@ const DrawingBoard = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+
+    context.fillStyle = "white";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
     context.strokeStyle = strokeStyle;
     context.lineWidth = pointerSize;
@@ -83,6 +151,10 @@ const DrawingBoard = () => {
           selectedColor={strokeStyle}
           changeColor={handleStrokeStyleChange}
         />
+      </div>
+      <div className="w-full flex items-center justify-start">
+        <Button onClick={handleUndo} label="Undo" />
+        <Button onClick={handleRedo} label="Redo" className="ml-4" />
       </div>
     </div>
   );
